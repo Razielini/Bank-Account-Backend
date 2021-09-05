2de4113233148719cfd580b607869ba638851f9c
Razielini <razielco@gmail.com>
2021-09-05 11:45:38 -0600

    :white_check_mark: Added Mocks Controllers

---
+++ __mocks__/controllers/auth.mock.ts
@@ -0,0 +1,9 @@
+export default {
+  user: {
+    id: '123456789',
+  },
+  jwt: {
+    secret: 'not_a_production_secret',
+    expiresIn: 30000,
+  },
+};
---
+++ __mocks__/controllers/index.ts
@@ -0,0 +1,5 @@
+import { default as auth } from './auth.mock';
+
+export default {
+  auth,
+};
