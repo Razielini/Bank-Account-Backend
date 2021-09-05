21a7798cb55c07b50fb41ed51e45400fe4432689
Razielini <razielco@gmail.com>
2021-09-05 13:23:32 -0600

    :white_check_mark: Added Person Model

--- src/database/mongodb/index.ts
+++ src/database/mongodb/index.ts
@@ -1,9 +1,11 @@
 import { default as user } from './models/user';
+import { default as person } from './models/person';
 import connection from './connection';
 
 export default {
   connection,
   models: {
     user,
+    person,
   },
 };
