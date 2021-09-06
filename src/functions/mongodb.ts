ebdd517a5b8c299cf254466b9397ac526f683f98
Razielini <razielco@gmail.com>
2021-09-05 21:53:45 -0600

    :recycle: Added LogicalDelete Function

--- src/functions/mongodb.ts
+++ src/functions/mongodb.ts
@@ -6,8 +6,12 @@ export const fetchDocument = ({ store, filter }: any) => store.findOne(filter);
 
 export const fetchDocuments = ({ store, filter, limit = findLimit }: any) =>
   store.find(filter).limit(parseInt(limit));
+
 export const createDocument = ({ store, data, options = null }: any) =>
   new store(data).save({ ...options });
 
 export const updateDocument = ({ store, data, options = null }: any) =>
   store.findByIdAndUpdate(data._id, { ...data }, { ...options });
+
+export const logicalDeleteDocument = ({ store, data, options = null }: any) =>
+  store.findByIdAndUpdate(data._id, { status: false }, { ...options, new: true });
